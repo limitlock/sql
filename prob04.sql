731208371f260712be41ea4df5b08b9c3655f29a
BIT <BIT@DESKTOP-5CCA2GS>
2018-03-08 10:22:30 +0900

    prob04_2_modify

--- prob04.sql
+++ prob04.sql
@@ -11,15 +11,15 @@ select count(a.emp_no)
 
 -- 2. 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. 
 
-  select a.emp_no as '사번', concat(a.first_name, ' ',a.last_name) as '이름', b.dept_no as '부서', max(c.salary) as '연봉'
+  select a.emp_no as '사번', concat(a.first_name, ' ',a.last_name) as '이름', b.dept_no as '부서', c.salary as '연봉'
     from employees a, dept_emp b, salaries c
    where a.emp_no = b.emp_no
      and a.emp_no = c.emp_no
      and b.to_date = '9999-01-01'
+     and c.to_date = '9999-01-01'
 group by dept_no
-  having max(c.salary)
-order by max(c.salary) desc;
-
+having max(c.salary)
+order by c.salary desc;
 
 
 -- 3. 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요 
@@ -55,7 +55,8 @@ where a.emp_no = b.emp_no
 and a.emp_no = c.emp_no
 and c.dept_no = d.dept_no
 and d.dept_name = e.m_dept_name
-and b.to_date = '9999-01-01';
+and b.to_date = '9999-01-01'
+and c.to_date ='9999-01-01';
 
 
 -- 5. 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
@@ -80,6 +81,7 @@ and b.to_date = '9999-01-01';
      and c.dept_no = d.dept_no
      and b.to_date = '9999-01-01'
      and e.to_date = '9999-01-01'
+     and c.to_date = '9999-01-01'
      and d.dept_name = g.department
 order by b.salary desc;
 
@@ -120,7 +122,7 @@ having avg(b.salary) >= (select max(a.avg_salary)
 -- 8. 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
 
-select e.dept_name as '부서이름', concat(first_name, ' ',last_name) as '사원이름', b.salary as '연봉', g.manager_name as'매니저 이름', g.manager_salary as '매니저 연봉'
+select e.dept_name as '부서이름', concat(a.first_name, ' ',a.last_name) as '사원이름', b.salary as '연봉', g.manager_name as'매니저 이름', g.manager_salary as '매니저 연봉'
 from employees a, salaries b, titles c, dept_emp d, departments e, (select concat(a.first_name, ' ',a.last_name) as 'manager_name', b.salary as 'manager_salary', d.dept_no as 'manager_department'
 																	  from employees a, salaries b, titles c, dept_emp d
 																	 where a.emp_no = b.emp_no
@@ -131,9 +133,9 @@ from employees a, salaries b, titles c, dept_emp d, departments e, (select conca
 where a.emp_no = b.emp_no
   and a.emp_no = d.emp_no
   and d.dept_no = e.dept_no
-  and b.salary > g.manager_salary
   and b.to_date = '9999-01-01'
-  and d.dept_no = g.manager_department;
+  and d.dept_no = g.manager_department
+  and b.salary > g.manager_salary;
 
 
 
